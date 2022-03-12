a90a7ce8ff45a67ae0269b1de292ba1f4359c8d6
RISHABH <gguptarishabh82@gmail.com>
2022-03-12 13:13:54 +0530

    Fetch Data For Rice

---
+++ FetchDataForRice.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11And12_Object_Oriented_Programs
+{
+    public class FetchDataForRice
+    {
+        public Rice Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ Rice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11And12_Object_Oriented_Programs
+{
+    public class Rice
+    {
+        public string Name { get; set; }
+        public string Weight { get; set; }
+        public string Price { get; set; }
+    }
+}
